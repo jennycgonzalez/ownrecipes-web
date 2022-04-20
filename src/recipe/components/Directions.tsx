d96fe9c31aa6bcb5ee9e84bfe58b8978249d4043
sepulzera <hartung_spam@gmx.de>
2022-04-20 21:13:30 +0200

    #29 Do not display direction subgroups with only 1 direction as list

--- src/recipe/components/Directions.tsx
+++ src/recipe/components/Directions.tsx
@@ -8,15 +8,28 @@ export interface IDirectionsProps {
   data: string;
 }
 
-function pushDirections(groups: Array<React.ReactNode>, heading: string, directions: Array<React.ReactNode>) {
+function pushDirections(groups: Array<React.ReactNode>, heading: string, directions: Array<string>) {
   if (directions.length === 0) return;
+
+  const directionsJsx = directions.length === 1 ? (
+    <div className='direction'>
+      {directions[0]}
+    </div>
+  ) : (
+    <ol className='directions'>
+      {directions.map(direction => (
+        <li className='direction' key={direction}>
+          {direction}
+        </li>
+      ))}
+    </ol>
+  );
+
   groups.push(
     <div key={heading} className='subgroup'>
       {/* eslint-disable-next-line react/jsx-one-expression-per-line */}
       {heading && <h3>{heading}:</h3>}
-      <ol className='directions'>
-        {directions}
-      </ol>
+      {directionsJsx}
     </div>
   );
 }
@@ -40,23 +53,19 @@ const Directions: React.FC<IDirectionsProps> = ({ data }: IDirectionsProps) => {
   const isMultiStep = data.includes('\n');
 
   const directionsGroups: Array<React.ReactNode> = [];
-  let directions: Array<React.ReactNode> = [];
+  let directions: Array<string> = [];
   let nextHeading = '';
 
   if (isMultiStep) {
     data.split('\n').filter(direction => direction.length > 0).forEach(direction => {
-          if (direction.endsWith(':')) {
+      if (direction.endsWith(':')) {
         pushDirections(directionsGroups, nextHeading, directions);
         nextHeading = direction.substring(0, direction.length - 1);
-            directions = [];
-          } else {
-            directions.push(
-              <li className='direction' key={direction}>
-                {direction}
-              </li>
-            );
-          }
-      });
+        directions = [];
+      } else {
+        directions.push(direction);
+      }
+    });
 
     pushDirections(directionsGroups, nextHeading, directions);
   }
@@ -69,7 +78,7 @@ const Directions: React.FC<IDirectionsProps> = ({ data }: IDirectionsProps) => {
       )}
       {hasData && (
         <>
-          {!isMultiStep && <div className='subgroup'>{data}</div>}
+          {!isMultiStep && <div className='direction'>{data}</div>}
           {isMultiStep && directionsGroups}
         </>
       )}
